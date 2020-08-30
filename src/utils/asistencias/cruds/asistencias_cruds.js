7d21c47cd0876b94d4d9305cac8fa5db706ef73f
LeandroCepeda <48310079+LeandroCepeda@users.noreply.github.com>
2020-08-30 00:02:22 -0300

    cargar asistencias terminado

--- src/utils/asistencias/cruds/asistencias_cruds.js
+++ src/utils/asistencias/cruds/asistencias_cruds.js
@@ -3,7 +3,16 @@ import axios from 'axios';
 import errorHandler from "../../error_handler";
 
 export async function addAsistenciasCrud(data, auth_token) {
-    return axios.post(`${config.api_url}/`, data, {
+
+    if (data.descripcion == "") {
+        data = {
+            alumno_curso: data.alumno_curso,
+            fecha: data.fecha,
+            asistio: data.asistio
+
+        }
+    }
+    return axios.post(`${config.api_url}/asistencias/`, data, {
         headers: {
             Authorization: `Token ${auth_token}`,
         },
@@ -22,12 +31,18 @@ export async function addAsistenciasCrud(data, auth_token) {
         });
 }
 
-export async function getAsistenciasCrud(auth_token) {
+
+//OBTENER LA LISTA DE ASISTENCIAS DE UN CURSO PARA UN FECHA DETERMINADA
+export async function getAsistenciasCrud(auth_token, curso_id, fecha_desde) {
     return axios
-        .get(`${config.api_url}/`, {
+        .get(`${config.api_url}/asistencias/list`, {
             headers: {
                 Authorization: `Token ${auth_token}`,
             },
+            params: {
+                curso: curso_id,
+                fecha_desde: fecha_desde
+            }
         })
         .then(json => {
             let response = {
@@ -40,10 +55,21 @@ export async function getAsistenciasCrud(auth_token) {
         .catch(error => errorHandler(error));
 }
 
-export async function editAsistenciasCrud(data, auth_token) {
+export async function editAsistenciasCrud(auth_token, data) {
+    let parsedData
+    if (data.descripcion == "" || data.descripcion == null) {
+        parsedData = {
+            asistio: data.asistio === "Asistió" ? 1 : 0
+        }
+    } else {
+        parsedData = {
+            asistio: data.asistio === "Asistió" ? 1 : 0,
+            descripcion: data.descripcion
+        }
+    }
 
     return axios
-        .patch(`${config.api_url}/`, data, {
+        .patch(`${config.api_url}/asistencias/${data.id_asistencia}/`, parsedData, {
             headers: {
                 Authorization: `Token ${auth_token}`,
             },
@@ -63,14 +89,20 @@ export async function editAsistenciasCrud(data, auth_token) {
 }
 
 
-export async function deleteAsistenciasCrud(data, auth_token) {
-    return axios.delete(`${config.api_url}/${data.id}/`, {
+export async function deleteAsistenciasCrud(auth_token, data) {
+    return axios.delete(`${config.api_url}/asistencias/${data.id_asistencia}/`, {
         headers: {
             Authorization: `Token ${auth_token}`,
         },
     })
-        .then((result) => {
-            return result;
+        .then((json) => {
+            let response = {
+                success: true,
+                result: json.data,
+            };
+
+            return response;
+
         }).catch((error) => {
             return errorHandler(error);
         });
--- src/utils/asistencias/services/asistencias_services.js
+++ src/utils/asistencias/services/asistencias_services.js
@@ -1,9 +1,9 @@
-import { getAsistenciasCrud, addAsistenciasCrud,editAsistenciasCrud, deleteAsistenciasCrud } from "../cruds/asistencias_cruds";
+import { getAsistenciasCrud, addAsistenciasCrud, editAsistenciasCrud, deleteAsistenciasCrud } from "../cruds/asistencias_cruds";
 import Alert from "react-s-alert";
 
 
-export async function getAsistenciasService(token) {
-    return await getAsistenciasCrud(token).then((result) => {
+export async function getAsistenciasService(token, curso_id, fecha_desde) {
+    return await getAsistenciasCrud(token, curso_id, fecha_desde).then((result) => {
         if (result.success) {
 
         } else {
@@ -20,7 +20,7 @@ export async function getAsistenciasService(token) {
 export async function addAsistenciasService(data, token) {
     return await addAsistenciasCrud(data, token).then((result) => {
         if (result.success) {
-            Alert.success("Asistencias creado correctamente", {
+            Alert.success("Asistencia creada correctamente", {
                 effect: "stackslide",
             });
         } else {
@@ -34,8 +34,8 @@ export async function addAsistenciasService(data, token) {
     })
 }
 
-export async function editAsistenciasService(data, token) {
-    return await editAsistenciasCrud(data, token).then((result) => {
+export async function editAsistenciasService(token, data) {
+    return await editAsistenciasCrud(token, data).then((result) => {
         if (result.success) {
             Alert.success("Asistencias editado correctamente", {
                 effect: "stackslide",
@@ -52,19 +52,19 @@ export async function editAsistenciasService(data, token) {
 }
 
 
-export async function deleteAsistenciasService(token,data){
-  return await deleteAsistenciasCrud(token,data).then((result)=>{
-      if (result.success) {
-        Alert.success("Materia eliminada correctamente", {
-          effect: "stackslide",
-        });
-        } else {
-          result.result.forEach((element) => {
-            Alert.error(element.message, {
-              effect: "stackslide",
+export async function deleteAsistenciasService(token, data) {
+    return await deleteAsistenciasCrud(token, data).then((result) => {
+        if (result.success) {
+            Alert.success("Asistencia eliminada correctamente", {
+                effect: "stackslide",
             });
-          });
+        } else {
+            result.result.forEach((element) => {
+                Alert.error(element.message, {
+                    effect: "stackslide",
+                });
+            });
         }
         return result;
-  })
+    })
 }
