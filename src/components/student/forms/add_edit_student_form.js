38d100748b7e0ecd86ffa494e5e93f86d0d41bcc
Mariano <marianolopezazeglio@gmail.com>
2020-09-01 00:23:51 -0300

    "erro" fecha

--- src/components/student/forms/add_edit_student_form.js
+++ src/components/student/forms/add_edit_student_form.js
@@ -290,7 +290,7 @@ const AddEditStudentForm = (props) => {
                                             placeholder="01/05/2020"
                                             onChange={(date) => handleChangeDate(date, 'birth')}
                                             inputVariant="outlined"
-                                            maxDate={new Date()}
+                                            maxDate={new Date('2003')}
                                             format="dd/MM/yyyy"
                                             invalidDateMessage="El formato de fecha es inválido"
                                             minDateMessage="La fecha no puede ser menor al día de hoy"
