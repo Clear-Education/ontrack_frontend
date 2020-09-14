0fca2cf01162efe6fa96b0f2be5ba8a750eef9e7
mariano <mariano.lopez@almundo.com>
2020-09-13 21:44:22 -0300

    merge arreglado

--- src/utils/error_handler.js
+++ src/utils/error_handler.js
@@ -89,100 +89,18 @@ const handleCrudErrors = (error) => {
       }
     }else{
       response = {
-        success: false,
-        unauthorized: false,
-        result: [
-          {
-            message: error.response.data.length > 0 ? error.response.data[0] : error.response.data.detail,
-          },
-        ],
-      };
-    } else if (error.response.status === 401) {
-      setTimeout(() => {
-        store.dispatch(forcedLogout());
-        window.location = "/";
-      }, 1500)
-      response = {
-        success: false,
-        unauthorized: true,
-        result: [
-          {
-            message: "Sesión expirada, vuelva a loguearse",
-          },
-        ],
-      };
-    } else if (error.response.status === 403) {
-      response = {
-        success: false,
-        unauthorized: false,
-        result: [
-          {
-            message: "No tenes permisos suficientes para realizar acción!",
-          },
-        ],
-      };
-    } else if (error.response.status === 404) {
-      response = {
-        success: false,
-        unauthorized: false,
-        result: [
-          {
-            message: error.response.data.detail
-          },
-        ],
-      };
-    } else if (error.response.status === 500) {
-      response = {
-        success: false,
-        unauthorized: false,
-        result: [
-          {
-            message: "Error interno del servidor",
-          },
-        ],
-      };
-    } else if (error.response.status === 409) {
-      response = {
-        success: false,
-        unauthorized: false,
-        error: 409,
-        result: [
-          {
-            message: error.response.data.detail
-          },
-        ],
-      };
-    } else if (error.response.status === 402) {
-      response = {
-        success: false,
-        unauthorized: false,
-        error: 402,
-        result: error.response.data.data,
-      };
-    } else {
-      response = {
-        success: false,
-        unauthorized: false,
-        result: [
-          {
-            message: "Error de conexión",
-          },
-        ],
-      };
+          success: false,
+          unauthorized: false,
+          result: [
+            {
+              message: "Error interno del servidor",
+            },
+          ],
+      }
     }
-  } else {
-    response = {
-      success: false,
-      unauthorized: false,
-      result: [
-        {
-          message: "Error interno del servidor",
-        },
-      ],
-    }
-  }
-
-  return response;
-};
-
-export default handleCrudErrors;
+  
+    return response;
+  };
+  
+  export default handleCrudErrors;
+  
